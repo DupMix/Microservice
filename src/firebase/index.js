5f17adacfe362b43ca8ce1b6a669467f0eb05b46
Greyson Elkins <greysonelkins@gmail.com>
2021-05-27 20:06:44 -0600

    Export firebase functions

---
+++ src/firebase/index.js
@@ -0,0 +1,8 @@
+export { saveTokensToFirebase, getTokensFromFirebase } from './tokens'
+export {
+  savePlaylistToFirebase,
+  getPlaylistsFromFirebase,
+  getLastWeeksPlaylistDynamically,
+  getThisWeeksPlaylistDynamically,
+  attemptSubmissionToFirebase,
+} from './playlists'
